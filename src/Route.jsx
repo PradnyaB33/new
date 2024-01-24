97fd7efe6d800581d8225d2da5c50547b4f82890
Dev4 <dev4.atsit@gmail.com>
2024-01-24 10:47:46 +0530

    base change

--- src/Route.jsx
+++ src/Route.jsx
@@ -14,11 +14,11 @@ import DashBoardHR from "./pages/DashBoard/DashBoardHR";
 import Dashboard from "./pages/DashBoard/Dashboard";
 import DashboardManger from "./pages/DashBoard/DashboardManger";
 import SuperAdmin from "./pages/DashBoard/SuperAdmin";
-import DeleteEmployee from "./pages/DeleteEmployee/DeleteEmployee";
 import AddDepartments from "./pages/Departments/AddDepartments";
 import DepartmentList from "./pages/Departments/DepartmentList";
 import Designation from "./pages/Designation/Designation";
 import AddEmployee from "./pages/Employee/AddEmployee";
+import DeleteEmployee from "./pages/Employee/DeleteEmployee";
 import EmployeeList from "./pages/Employee/EmployeeList";
 import Home from "./pages/Home/Home";
 import LeaveRequisition from "./pages/LeaveRequisition/LeaveRequisition";
@@ -273,7 +273,7 @@ function RequireAuth({ children, permission }) {
     return <UnAuthorized />;
   }
 
-  return user && isPermission ? children : navigate("/");
+  return user && isPermission ? children : <Navigate to={"/"} />;
 }
 
 //   : user?.profile?.length < 2 ? (
