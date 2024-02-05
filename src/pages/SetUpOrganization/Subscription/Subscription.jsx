2d3c40633cc0ef912a7ef4fad57ef7f647827a61
dev-2-atsit <dev2.atsit@gmail.com>
2024-02-05 11:09:39 +0530

    update

--- src/pages/SetUpOrganization/Subscription/Subscription.jsx
+++ src/pages/SetUpOrganization/Subscription/Subscription.jsx
@@ -1,76 +1,51 @@
 import { ManageAccountsOutlined } from "@mui/icons-material";
-import axios from "axios";
-import React, { useContext, useState } from "react";
-import { useMutation, useQuery, useQueryClient } from "react-query";
-import { useParams } from "react-router-dom";
-import { TestContext } from "../../../State/Function/Main";
-import { UseContext } from "../../../State/UseState/UseContext";
+import React from "react";
 import Setup from "../Setup";
 
 const Subscription = () => {
-  const { cookies } = useContext(UseContext);
-  const authToken = cookies["aeigs"];
-  const queryClient = useQueryClient();
-  const { handleAlert } = useContext(TestContext);
-  const { organisationId } = useParams();
-
-  const [deleteConfirmation, setDeleteConfirmation] = useState(null);
-
-  // Modal states and function
-  const [open, setOpen] = React.useState(false);
-  const [editModalOpen, setEditModalOpen] = useState(false);
-  const [empTypeId, setempTypeId] = useState(null);
+  // const { cookies } = useContext(UseContext);
+  // const authToken = cookies["aeigs"];
+  // const queryClient = useQueryClient();
 
   // const handleClickOpen = (scrollType) => () => {
   //   setOpen(true);
   //   setScroll(scrollType);
   // };
 
-  const handleOpen = (scrollType) => {
-    setOpen(true);
-    setempTypeId(null);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-    setempTypeId(null);
-    setEditModalOpen(false);
-  };
-
   // Get Query
-  const { data: empList, isLoading } = useQuery(
-    ["empTypes", organisationId],
-    async () => {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API}/route/employment-types-organisation/${organisationId}`,
-        {
-          headers: {
-            Authorization: authToken,
-          },
-        }
-      );
-      return response.data;
-    }
-  );
+  // const { data: empList, isLoading } = useQuery(
+  //   ["empTypes", organisationId],
+  //   async () => {
+  //     const response = await axios.get(
+  //       `${process.env.REACT_APP_API}/route/employment-types-organisation/${organisationId}`,
+  //       {
+  //         headers: {
+  //           Authorization: authToken,
+  //         },
+  //       }
+  //     );
+  //     return response.data;
+  //   }
+  // );
 
-  const deleteMutation = useMutation(
-    (id) =>
-      axios.delete(
-        `${process.env.REACT_APP_API}/route/employment-types/${id}`,
-        {
-          headers: {
-            Authorization: authToken,
-          },
-        }
-      ),
-    {
-      onSuccess: () => {
-        // Invalidate and refetch the data after successful deletion
-        queryClient.invalidateQueries("empTypes");
-        handleAlert(true, "success", "Employeement types deleted succesfully");
-      },
-    }
-  );
+  // const deleteMutation = useMutation(
+  //   (id) =>
+  //     axios.delete(
+  //       `${process.env.REACT_APP_API}/route/employment-types/${id}`,
+  //       {
+  //         headers: {
+  //           Authorization: authToken,
+  //         },
+  //       }
+  //     ),
+  //   {
+  //     onSuccess: () => {
+  //       // Invalidate and refetch the data after successful deletion
+  //       queryClient.invalidateQueries("empTypes");
+  //       handleAlert(true, "success", "Employeement types deleted succesfully");
+  //     },
+  //   }
+  // );
 
   return (
     <>
