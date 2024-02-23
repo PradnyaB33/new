c905acf2b03b99466d3639bcfa49fe7036a0054d
dev-2-atsit <dev2.atsit@gmail.com>
2024-02-23 17:47:39 +0530

    main-package

---
+++ src/pages/Billing/components/add-packages.jsx
@@ -0,0 +1,131 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PostAdd } from "@mui/icons-material";
+import { Box, Button, Modal } from "@mui/material";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import AuthInputFiled from "../../../components/InputFileds/AuthInputFiled";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  p: 4,
+  width: 450,
+};
+const MiniPackagesForm = ({ handleClose, open, setPackage }) => {
+  const packageSchema = z.object({
+    package: z.enum([
+      "basicPackageCount",
+      "remotePunchingPackageCount",
+      "performancePackageCount",
+      "basicTrainingPackageCount",
+      "communicationPackageCount",
+      "loanManagementPackageCount",
+      "cateringFoodPackageCount",
+    ]),
+  });
+  let defaultValues = {};
+
+  const { control, formState, handleSubmit } = useForm({
+    defaultValues,
+    resolver: zodResolver(packageSchema),
+  });
+  const { errors, isDirty } = formState;
+  function onSubmit(data) {
+    console.log(`🚀 ~ file: manage-package-form.jsx:34 ~ data:`, [
+      data.package,
+      "0",
+    ]);
+    setPackage((prevData) => [...prevData, [data.package, "0"]]);
+    handleClose();
+  }
+  return (
+    <Modal
+      keepMounted={false}
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+        sx={style}
+        className="border-none !z-10 shadow-md outline-none rounded-md gap-2 flex flex-col"
+      >
+        <h1 className="text-xl pl-2 font-semibold font-sans">Add package</h1>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-4"
+          noValidate
+        >
+          <AuthInputFiled
+            name="package"
+            icon={PostAdd}
+            control={control}
+            type="naresh-select"
+            placeholder="Select package "
+            label="Select package  *"
+            errors={errors}
+            error={errors.package}
+            options={[
+              {
+                value: "basicPackageCount",
+                label: "Basic package 35₹ /employee",
+              },
+              {
+                value: "remotePunchingPackageCount",
+                label: "Remote punching 35₹ /employee",
+              },
+              {
+                value: "performancePackageCount",
+                label: "Performance evaluation 35₹ /employee",
+              },
+
+              {
+                value: "basicTrainingPackageCount",
+                label: "Basic training package 35₹ /employee",
+              },
+              {
+                value: "communicationPackageCount",
+                label: "Communication package 35₹ /employee",
+              },
+              {
+                value: "loanManagementPackageCount",
+                label: "Loan management package 35₹ /employee",
+              },
+              {
+                value: "cateringFoodPackageCount",
+                label: "Catering Food package 35₹ /employee",
+              },
+              {
+                value: "analyticsAndReportingPackageCount",
+                label: "Analytics and reporting package 35₹ /employee",
+              },
+              {
+                value: "skillMatrixPackageCount",
+                label: "Analytics and reporting package 35₹ /employee",
+              },
+            ]}
+          />
+          <Button variant="contained" disabled={!isDirty} type="submit">
+            Add
+          </Button>
+        </form>
+      </Box>
+    </Modal>
+  );
+};
+
+export default MiniPackagesForm;
+function transformString(inputString, excludedWords = []) {
+  return inputString
+    .split(/(?=[A-Z])/)
+    .map((word) => {
+      console.log(`🚀 ~ file: manage-package-form.jsx:74 ~ word:`, word);
+      const formattedWord = word.charAt(0).toUpperCase() + word.slice(1);
+      return excludedWords.includes(formattedWord) ? "" : formattedWord;
+    })
+    .join(" ")
+    .trim();
+}
--- src/pages/Billing/components/manage-package-form.jsx
+++ src/pages/Billing/components/manage-package-form.jsx
@@ -1,5 +1,11 @@
-import { Box, Modal } from "@mui/material";
-import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Add, FilterCenterFocusOutlined } from "@mui/icons-material";
+import { Box, Button, Fab, Modal } from "@mui/material";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import AuthInputFiled from "../../../components/InputFileds/AuthInputFiled";
+import MiniPackagesForm from "./add-packages";
 const style = {
   position: "absolute",
   top: "50%",
@@ -9,7 +15,27 @@ const style = {
   p: 4,
 };
 const PackageForm = ({ handleClose, open, packages }) => {
-  console.log(`🚀 ~ file: manage-package-form.jsx:12 ~ packages:`, packages);
+  const [mainPackages, setmainPackages] = useState(packages);
+  console.log(
+    `🚀 ~ file: manage-package-form.jsx:19 ~ mainPackages:`,
+    mainPackages
+  );
+  const [close, setClose] = useState(false);
+  const packageSchema = z.object(
+    Object.fromEntries(mainPackages?.map((doc) => [doc[0], z.string()]))
+  );
+  let defaultValues = Object.fromEntries(
+    mainPackages?.map((doc) => [doc[0], doc[1]])
+  );
+
+  const { control, formState, handleSubmit } = useForm({
+    defaultValues,
+    resolver: zodResolver(packageSchema),
+  });
+  const { errors, isDirty } = formState;
+  function onSubmit(data) {
+    console.log(`🚀 ~ file: manage-package-form.jsx:34 ~ data:`, data);
+  }
   return (
     <Modal
       keepMounted={false}
@@ -22,13 +48,59 @@ const PackageForm = ({ handleClose, open, packages }) => {
         sx={style}
         className="border-none !z-10 shadow-md outline-none rounded-md gap-2 flex flex-col"
       >
-        <h1 className="text-xl pl-2 font-semibold font-sans">Add leave type</h1>
-        {/* <form onSubmit={handleSubmit(onSubmit)} noValidate>
-       
-        </form> */}
+        <h1 className="text-xl pl-2 font-semibold font-sans">
+          Manage subscription
+        </h1>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-4"
+          noValidate
+        >
+          {mainPackages.map((doc) => {
+            return (
+              <AuthInputFiled
+                name={doc[0]}
+                icon={FilterCenterFocusOutlined}
+                control={control}
+                type="text"
+                placeholder={transformString(doc[0])}
+                label={`${transformString(doc[0])} *`}
+                errors={errors}
+                error={errors[doc[0]]}
+              />
+            );
+          })}
+          <Fab
+            onClick={() => setClose(true)}
+            type="button"
+            color="primary"
+            aria-label="add"
+          >
+            <Add />
+          </Fab>
+          <Button variant="contained" disabled={!isDirty} type="submit">
+            Submit
+          </Button>
+        </form>
+        <MiniPackagesForm
+          open={close}
+          handleClose={() => setClose(false)}
+          setPackage={setmainPackages}
+        />
       </Box>
     </Modal>
   );
 };
 
 export default PackageForm;
+function transformString(inputString, excludedWords = []) {
+  return inputString
+    .split(/(?=[A-Z])/)
+    .map((word) => {
+      console.log(`🚀 ~ file: manage-package-form.jsx:74 ~ word:`, word);
+      const formattedWord = word.charAt(0).toUpperCase() + word.slice(1);
+      return excludedWords.includes(formattedWord) ? "" : formattedWord;
+    })
+    .join(" ")
+    .trim();
+}
