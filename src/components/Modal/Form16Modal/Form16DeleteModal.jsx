68fa30364b5db55bb0b07d6aa43f46a0ae7b21b6
dumbremegha <dumbremegha3232@gmail.com>
2024-02-24 17:24:07 +0530

    file saved

--- src/components/Modal/Form16Modal/Form16DeleteModal.jsx
+++ src/components/Modal/Form16Modal/Form16DeleteModal.jsx
@@ -8,17 +8,14 @@ const style = {
   bgcolor: "background.paper",
   p: 4,
 };
-const Form16Download = ({ handleClose, open, employeeId, organizationId }) => {
-  let pdfPath =
-    "https://aegis-dev.s3.ap-south-1.amazonaws.com/65d86909299bc72288c0b994-salaryslip%20%289%29.pdf";
-  const handleDownload = () => {
-    // You can use any method to trigger the download, such as creating an invisible link and clicking it
-    const link = document.createElement("a");
-    link.href = pdfPath;
-    link.download = "Form16.pdf";
-    link.click();
-  };
-
+const Form16DeleteModal = ({
+  handleClose,
+  open,
+  employeeId,
+  organizationId,
+}) => {
+  console.log(employeeId);
+  console.log(organizationId);
   return (
     <>
       <Modal
@@ -33,28 +30,17 @@ const Form16Download = ({ handleClose, open, employeeId, organizationId }) => {
         >
           <div className="flex justify-between py-4 items-center  px-4">
             <h1 className="text-xl pl-2 font-semibold font-sans">
-              Download Form 16
+              Delete Form 16
             </h1>
           </div>
-          <object
-            type="application/pdf"
-            width="100%"
-            height="500px"
-            data="https://aegis-dev.s3.ap-south-1.amazonaws.com/65d86909299bc72288c0b994-salaryslip%20%289%29.pdf"
-            className="w-full "
-          />
 
           <div className="px-5 space-y-4 mt-4">
             <div className="flex gap-4  mt-4 mr-4 justify-end mb-4 ">
               <Button onClick={handleClose} color="error" variant="outlined">
                 Cancel
               </Button>
-              <Button
-                onClick={handleDownload}
-                variant="contained"
-                color="primary"
-              >
-                Form 16
+              <Button variant="contained" color="primary">
+                Delete Form 16
               </Button>
             </div>
           </div>
@@ -64,4 +50,4 @@ const Form16Download = ({ handleClose, open, employeeId, organizationId }) => {
   );
 };
 
-export default Form16Download;
+export default Form16DeleteModal;
