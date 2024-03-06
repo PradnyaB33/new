9933478f139d7c119540f88c9645d0e77a13dda3
dev-2-atsit <dev2.atsit@gmail.com>
2024-03-06 08:05:54 +0530

    fixed

---
+++ src/hooks/QueryHook/Delegate-Super-Admin/mutation.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useMutation } from "react-query";
+import useGetUser from "../../Token/useUser";
+
+const useDelegateSuperAdmin = () => {
+  const { authToken } = useGetUser();
+
+  const createDelegate = async (data) => {
+    console.log(`🚀 ~ file: mutation.jsx:13 ~ data:`, data);
+    const response = await axios.patch(
+      `${process.env.REACT_APP_API}/route/employee/delegate`,
+      data,
+      {
+        headers: {
+          Authorization: authToken,
+        },
+      }
+    );
+    console.log(`🚀 ~ file: mutation.jsx:23 ~ response:`, response);
+    return response.data;
+  };
+
+  const addDelegateMutation = useMutation({
+    mutationFn: createDelegate,
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (data) => {
+      console.log(data);
+    },
+  });
+  return {
+    addDelegateMutation,
+  };
+};
+
+export default useDelegateSuperAdmin;
