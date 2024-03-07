1e592815d785358b0d5af3f9417603e72f3b4fe5
dev-3-atsit <dev3.atsit@gmail.com>
2024-03-07 17:16:58 +0530

    punchIn

--- src/pages/Test/testMap.jsx
+++ src/pages/Test/testMap.jsx
@@ -2,18 +2,45 @@ import { GoogleMap, Marker, Polyline } from "@react-google-maps/api";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import useGetUser from "../../hooks/Token/useUser";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
 const containerStyle = {
-  width: "100%",
-  height: "91.5vh",
+  width: "70%",
+  height: "91.8vh",
+  border: "2px solid gray",
 };
-
-console.log(window.google.maps);
 
 const TestMap = () => {
   const [waypoints, setWaypoints] = useState([]);
   const [currentLocation, setCurrentLocation] = useState(null);
   const { authToken } = useGetUser();
+  const [selectedDayLocation, setSelectedDayLocation] = useState();
+  const [dates, setDates] = useState([]);
+  const [selectedEmpId, setSelectedEmpId] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
+  const [allEmp, setAllEmp] = useState([]);
+
+  const fetchReportees = async () => {
+    try {
+      const resp = await axios.get(
+        `${process.env.REACT_APP_API}/route/employee/countofEmployees`,
+        {
+          headers: {
+            Authorization: authToken,
+          },
+        }
+      );
+      setAllEmp(resp.data.data[0].reporteeIds);
+      console.log(resp.data.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchReportees();
+    // eslint-disable-next-line
+  }, []);
 
   useEffect(() => {
     (async () => {
@@ -26,7 +53,6 @@ const TestMap = () => {
             },
           }
         );
-        console.log(response.data.data);
 
         // Handle potential data fetching errors
         if (response.data.error) {
@@ -126,15 +152,123 @@ const TestMap = () => {
     lng: waypoints[waypoints.length - 1]?.lng,
   };
 
-  console.log(waypoints);
+  const handleChange = (event, type) => {
+    if (type === "employee") {
+      const employeeId = event.target.value;
+      console.log(employeeId);
+      setSelectedEmpId(employeeId);
+      getEmp(employeeId);
+    } else if (type === "date") {
+      const date = event.target.value;
+      console.log(date);
+      setSelectedDate(date);
+      getLocation(date);
+    }
+  };
+
+  const getEmp = async (id) => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API}/route/punch/getForEmp/${id}`,
+        {
+          headers: {
+            Authorization: authToken,
+          },
+        }
+      );
+
+      setSelectedDayLocation(response?.data.punch.days);
+      const formattedDates = response?.data.punch.days.map((data) => {
+        const date = new Date(data.createdAt);
+        const day = date.getDate().toString().padStart(2, "0");
+        const month = (date.getMonth() + 1).toString().padStart(2, "0");
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+      });
+      setDates(formattedDates);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const getLocation = (id) => {
+    const selectedPath = selectedDayLocation[id];
+    console.log(selectedDayLocation);
+    const newWaypoints = selectedPath.location.map((punch) => ({
+      lat: parseFloat(punch.lat),
+      lng: parseFloat(punch.lng),
+    }));
+
+    const smoothedWaypoints = smoothWaypoints(newWaypoints, 3);
+    setWaypoints(smoothedWaypoints);
+    setCurrentLocation({
+      lat: waypoints[waypoints.length - 1].lat,
+      lng: waypoints[waypoints.length - 1].lng,
+    });
+  };
 
   return (
-    <div>
+    <div className="w-full flex justify-between">
+      <div className=" z-50 p-10 flex flex-col mt-6 w-[30vw] bg-white gap-4 ">
+        <div className="w-full bg-white">
+          <FormControl fullWidth size="small">
+            <InputLabel id="demo-simple-select-label">
+              Select Employee
+            </InputLabel>
+            <Select
+              labelId="employee-select-label"
+              id="employee-select"
+              value={selectedEmpId}
+              label="Select Employee"
+              onChange={(event) => handleChange(event, "employee")}
+            >
+              {allEmp.length > 0 ? (
+                allEmp.map((item, idx) => (
+                  <MenuItem key={idx} value={item._id}>
+                    {item.first_name}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem value="">No Employee Found</MenuItem>
+              )}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="w-full bg-white">
+          <FormControl fullWidth size="small">
+            <InputLabel id="demo-simple-select-label">Select Date</InputLabel>
+            <Select
+              labelId="date-select-label"
+              id="date-select"
+              value={selectedDate}
+              label="Select Date"
+              onChange={(event) => handleChange(event, "date")}
+            >
+              {dates.length > 0 ? (
+                dates.map((date, idx) => (
+                  <MenuItem key={idx} value={idx}>
+                    {date}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem value="">No Data Found</MenuItem>
+              )}
+            </Select>
+          </FormControl>
+          <div>
+            {waypoints?.length > 0 && (
+              <p className=" z-[99999999]  mt-4 font-semibold">
+                Total Distance Travelled : {totalDistance.toFixed(2)} Kilometers
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
       <GoogleMap
         googleMapsApiKey="AIzaSyDaA2q3L--j40-GgojdeFSJ4RywKGtFQ2k"
         mapContainerStyle={containerStyle}
         onLoad={() => console.log("Map loaded")}
-        zoom={15}
+        zoom={12}
         center={currentLocation}
         options={{
           zoomControl: false,
@@ -153,31 +287,44 @@ const TestMap = () => {
         {waypoints?.length > 0 && (
           <>
             <Marker
-              icon={{
-                url: "https://img.icons8.com/ios/50/000000/marker.png",
-                scaledSize: new window.google.maps.Size(35, 35),
+              label={{
+                text: "Source",
+                fontWeight: "bold",
+                fontSize: "1rem",
               }}
-              label={{ text: "source" }}
               position={center}
-              style={{ filter: "hue-rotate(180deg)" }}
+              icon={{
+                url: "https://maps.gstatic.com/mapfiles/ms2/micons/blue.png",
+                scaledSize: new window.google.maps.Size(40, 40),
+                fillColor: "blue",
+                fillOpacity: 1,
+                strokeColor: "blue",
+                strokeWeight: 2,
+              }}
             />
             <Polyline
               path={waypoints}
               options={{ strokeColor: "#7a3eff", strokeWeight: 5 }}
             />
             <Marker
-              label={{ text: "destination", color: "black" }}
+              label={{
+                text: "Destination",
+                fontWeight: "bold",
+                fontSize: "1rem",
+              }}
               position={destination}
+              icon={{
+                url: "https://maps.gstatic.com/mapfiles/ms2/micons/red.png",
+                scaledSize: new window.google.maps.Size(40, 40),
+                fillColor: "red",
+                fillOpacity: 1,
+                strokeColor: "blue",
+                strokeWeight: 2,
+              }}
             />
           </>
         )}
       </GoogleMap>
-
-      {waypoints?.length > 0 && (
-        <p className="absolute top-24 z-[99999999] bg-black text-gray-50">
-          Total Distance Travelled : {totalDistance.toFixed(2)} Kilometers
-        </p>
-      )}
     </div>
   );
 };
