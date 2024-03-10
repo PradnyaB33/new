e038176d66e4643f14b5787c6e63ec99a3cf3f95
dev-3-atsit <dev3.atsit@gmail.com>
2024-03-11 00:57:43 +0530

    NotificationsIntegrated

--- src/pages/Employee-Confirm/components/mapped-punches.jsx
+++ src/pages/Employee-Confirm/components/mapped-punches.jsx
@@ -1,26 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import useGetSinglePunch from "../../../hooks/QueryHook/Remote-Punch/components/hook";
+
+const calculateDistance = (lat1, lon1, lat2, lon2) => {
+  const R = 6371;
+  const dLat = (lat2 - lat1) * (Math.PI / 180);
+  const dLon = (lon2 - lon1) * (Math.PI / 180);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1 * (Math.PI / 180)) *
+      Math.cos(lat2 * (Math.PI / 180)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c;
+
+  return distance;
+};
 
 const MappedPunches = ({ Id }) => {
   const { data, isLoading } = useGetSinglePunch({ Id });
   console.log(`🚀 ~ file: mapped-punches.jsx:6 ~ data:`, data);
+
   return (
-    <>
-      {" "}
-      {/* {data?.punchData?.map((doc) => ( */}
-      <div className="w-full h-auto bg-[#e2f1ff]">
-        <div className="flex w-full items-center h-full p-5">
-          <div className="mr-3">
-            <div className="w-[55px] h-[55px] bg-black rounded-full"></div>
+    <div className="w-full h-80 overflow-y-auto">
+      {data?.punchData?.punchData?.map((doc, idx) => {
+        let distance = 0;
+        let totalDistance = 0;
+        if (doc.data && idx < doc.data.length - 1) {
+          const currentData = doc.data[idx];
+          const nextData = doc.data[idx + 1];
+          distance =
+            calculateDistance(
+              currentData.lat,
+              currentData.lng,
+              nextData.lat,
+              nextData.lng
+            ).toFixed(2) + " km";
+          totalDistance += distance;
+        }
+
+        return (
+          <div
+            key={idx}
+            className="w-full h-auto bg-[#e2f1ff] flex flex-col mb-3"
+          >
+            <div className="flex w-full items-center h-full p-5">
+              <div className="mr-3">
+                <div className="w-[55px] h-[55px] bg-black rounded-full"></div>
+              </div>
+              <div className="pl-5 flex flex-col ">
+                <h1>
+                  Start Time:{" "}
+                  {new Date(doc.data[0]?.createdAt).toLocaleTimeString()}
+                </h1>
+                <h1>
+                  End Time:{" "}
+                  {doc.data &&
+                  doc.data.length > 0 &&
+                  doc.data[doc.data.length - 1]?.createdAt
+                    ? new Date(
+                        doc.data[doc.data.length - 1]?.createdAt
+                      ).toLocaleTimeString()
+                    : "N/A"}
+                </h1>
+                <h1>Distance Travelled: {distance}</h1>
+              </div>
+            </div>
           </div>
-          <div className="pl-5 flex flex-col ">
-            <h1>Location: Pune Station</h1>
-            <h1>Distance Travelled : 2 km</h1>
-          </div>
-        </div>
-      </div>
-      {/* // ))} */}
-    </>
+        );
+      })}
+    </div>
   );
 };
 
