dff509378a8c45daa65266560e7d8fa4956cae3f
dev2.atsit <dev2.atsit@gmail.com>
2024-03-12 13:31:28 +0530

    fix main

--- src/hooks/QueryHook/Location/independant-use-query.jsx
+++ src/hooks/QueryHook/Location/independant-use-query.jsx
@@ -1,16 +1,31 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useContext } from "react";
+import { useMutation, useQuery } from "react-query";
+import { TestContext } from "../../../State/Function/Main";
 import useGetUser from "../../Token/useUser";
 import useLocationMutation from "./mutation";
 import useSelfieStore from "./zustand-store";
 
 const useStartPunch = () => {
   const { authToken } = useGetUser();
-  const { punchObjectId, start, setLocation } = useSelfieStore();
+  const {
+    punchObjectId,
+    start,
+    setLocation,
+    setTemporaryArray,
+    temporaryArray,
+    setId,
+  } = useSelfieStore();
+  console.log(
+    `🚀 ~ file: independant-use-query.jsx:18 ~ temporaryArray:`,
+    temporaryArray
+  );
   const { getUserLocation } = useLocationMutation();
   const { data: objectData, mutate } = getUserLocation;
+  const { handleAlert } = useContext(TestContext);
   const fetchLocationData = async () => {
     mutate();
+    startGeoLocationWatch.mutate();
     const { latitude, longitude } = objectData;
     setLocation({ lat: latitude, lng: longitude });
     const payload = {
@@ -35,6 +50,46 @@ const useStartPunch = () => {
     refetchIntervalInBackground: true,
     onSuccess: (data) => {},
   });
+  const getNavigatorData = async () => {
+    const id = navigator.geolocation.watchPosition(
+      (positionCallback) => {
+        const { latitude, longitude } = positionCallback.coords;
+        console.log(
+          `🚀 ~ file: independant-use-query.jsx:45 ~ latitude, longitude :`,
+          latitude,
+          longitude
+        );
+        setTemporaryArray(latitude, longitude);
+        setLocation({ lat: latitude, lng: longitude });
+      },
+      () => {
+        handleAlert(
+          true,
+          "error",
+          "Error Getting GeoLocation please reload webpage"
+        );
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    );
+    return id;
+  };
+
+  const startGeoLocationWatch = useMutation({
+    mutationFn: getNavigatorData,
+    onSuccess: (data) => {
+      console.log(`🚀 ~ file: independant-use-query.jsx:58 ~ data:`, data);
+      setId(data);
+    },
+    onError: (data) => {
+      console.error(data);
+    },
+  });
+  console.log("startGeoLocationWatch", startGeoLocationWatch);
+
   return { data, refetch };
 };
 
--- src/pages/Remote-Punching-Employee/components/stop-remote-punching.jsx
+++ src/pages/Remote-Punching-Employee/components/stop-remote-punching.jsx
@@ -2,18 +2,24 @@ import { Stop } from "@mui/icons-material";
 import { Fab } from "@mui/material";
 import React, { useEffect } from "react";
 import useStartPunch from "../../../hooks/QueryHook/Location/independant-use-query";
+import useSelfieStore from "../../../hooks/QueryHook/Location/zustand-store";
 
 const StopRemotePunch = ({ setStart }) => {
   const { refetch } = useStartPunch();
+  const { id } = useSelfieStore();
   useEffect(() => {
     refetch();
   }, [refetch]);
+  const stopRemotePunching = () => {
+    setStart(false);
+    navigator.geolocation.clearWatch(id);
+  };
 
   return (
     <Fab
       variant="extended"
       className="!absolute bottom-12 right-12 !bg-primary !text-white"
-      onClick={() => setStart(false)}
+      onClick={stopRemotePunching}
     >
       <Stop sx={{ mr: 1 }} className={`animate-pulse text-white`} />
       Stop Remote Punching
