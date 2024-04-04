460ea261b498a92f0c733fe27b0238ec8387484a
Megha Dumbre <meghadu@ats.com>
2024-04-04 17:08:01 +0530

    bug fixed loan

--- src/pages/LoanManagement/LoanManagementPieChart.jsx
+++ src/pages/LoanManagement/LoanManagementPieChart.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
-
+import {   Typography } from "@mui/material";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const LoanManagementPieChart = ({ totalPaidAmount, totalPendingAmount }) => {
   const data = {
-    labels: ["Amount Paid", "Amount Pending"],
+    labels: ["Paid Amount", "Pending Amount"],
     datasets: [
       {
         data: [totalPaidAmount, totalPendingAmount],
-        backgroundColor: ["orangered", "purple"],
+        backgroundColor: ["#4169E1", "#FF6347"],
       },
     ],
   };
@@ -26,9 +26,17 @@ const LoanManagementPieChart = ({ totalPaidAmount, totalPendingAmount }) => {
   };
 
   return (
-    <div>
-      <Pie data={data} options={options} className=" w-full max-w-96" />
-    </div>
+    
+    <div  style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)", width: "900px", height: "400px"  , marginLeft :"20%" ,paddingTop : "5%" }}>
+    <Typography variant="h4" className="text-center  mb-6 mt-2">
+      Loan Application
+    </Typography>
+    <p className="text-xs text-gray-600  text-center">
+      Here you can see the status of loan amount.
+    </p>
+    <Pie data={data} options={options} className="w-full max-w-96" style={{ marginLeft: "35%"  , }} />
+  </div>
+  
   );
 };
 
