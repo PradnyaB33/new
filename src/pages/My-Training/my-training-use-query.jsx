9c56fe557b5d947812975c0bc6e4c6154037bee6
dev-2-atsit <dev2.atsit@gmail.com>
2024-04-13 08:45:37 +0530

    manager completed

--- src/pages/My-Training/my-training-use-query.jsx
+++ src/pages/My-Training/my-training-use-query.jsx
@@ -16,7 +16,7 @@ const useTrainingFormEmployee = () => {
 
   const getEmployee = async () => {
     const response = await axios.get(
-      `${process.env.REACT_APP_API}/route/training/get-recent-training/${decodedToken?.user?.organizationId}?page=${page}?trainingName=${trainingName}?trainingDepartment=${trainingDepartment}?trainingType=${trainingType}`,
+      `${process.env.REACT_APP_API}/route/training/get-recent-training/${decodedToken?.user?.organizationId}?page=${page}?trainingName=${trainingName}?trainingDepartment=${trainingDepartment?.value}?trainingType=${trainingType?.value}`,
       {
         headers: {
           Authorization: authToken,
@@ -31,8 +31,8 @@ const useTrainingFormEmployee = () => {
       `get-employee-data`,
       page,
       trainingName,
-      trainingDepartment,
-      trainingType,
+      trainingDepartment?.value,
+      trainingType?.value,
     ],
     queryFn: getEmployee,
     onSuccess: (data) => {
