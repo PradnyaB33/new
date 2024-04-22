e2e9a1eb3f34d24b9186aa3e2867d3fcc2678fc1
dev2.atsit <dev2.atsit@gmail.com>
2024-04-22 17:56:31 +0530

    fixe

--- src/pages/AddOrganisation/components/step-3.jsx
+++ src/pages/AddOrganisation/components/step-3.jsx
@@ -79,7 +79,7 @@ const Step3 = ({ nextStep }) => {
             { value: "Phone_Pay", label: "Phone_Pay" },
             { value: "RazorPay", label: "RazorPay" },
           ]}
-          descriptionText={"Additional 2% charges on every transaction"}
+          descriptionText={"Additional 2% charges on razorpay transaction"}
         />
         <Button type="submit" variant="contained" className="!w-max !mx-auto">
           Confirm & Pay
