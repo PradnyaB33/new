124610db556e4ffc9fb3e588654d62dc5e618d47
dev-3-atsit <dev3.atsit@gmail.com>
2024-04-22 22:54:20 +0530

    allFixed

--- src/pages/Test/RemoteEmployee/components/MiniForm.jsx
+++ src/pages/Test/RemoteEmployee/components/MiniForm.jsx
@@ -26,6 +26,7 @@ const MiniForm = ({ setArray, setOpenModal, center, setCenter, today }) => {
     }),
     start: z.string(),
     end: z.string(),
+    distance: z.string(),
   });
 
   const { control, formState, handleSubmit, reset } = useForm({
@@ -46,6 +47,7 @@ const MiniForm = ({ setArray, setOpenModal, center, setCenter, today }) => {
       },
       start: undefined,
       end: undefined,
+      distance: undefined,
     },
     resolver: zodResolver(formSchema),
   });
@@ -57,6 +59,8 @@ const MiniForm = ({ setArray, setOpenModal, center, setCenter, today }) => {
     const endDateTime = data.end
       ? moment(`${today} ${data?.end}`, "YYYY-MM-DD HH:mm")
       : null;
+
+    console.log("this is  my data for misspunch", data);
 
     const formattedData = {
       ...data,
@@ -124,6 +128,16 @@ const MiniForm = ({ setArray, setOpenModal, center, setCenter, today }) => {
           label="End Time *"
           errors={errors}
           wrapperMessage={"Note: End time for missed punch"}
+        />
+        <AuthInputFiled
+          className="w-full"
+          name="distance"
+          control={control}
+          type="number"
+          placeholder="Enter the distance"
+          label="Total Distance *"
+          errors={errors}
+          wrapperMessage={"Note: Total distance for missed punch"}
         />
       </div>
       <div className="w-full flex justify-center mt-4">
