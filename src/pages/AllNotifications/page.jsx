1514152c6bb532dcec008089eea70ef9bb9c83ff
Megha Dumbre <meghadu@ats.com>
2024-04-24 14:26:25 +0530

    AGS-449 fixed bug solved

--- src/pages/AllNotifications/page.jsx
+++ src/pages/AllNotifications/page.jsx
@@ -3,6 +3,7 @@ import useMissedPunchNotificationCount from "../../hooks/QueryHook/notification/
 import useLeaveNotificationHook from "../../hooks/QueryHook/notification/leave-notification/hook";
 import usePunchNotification from "../../hooks/QueryHook/notification/punch-notification/hook";
 import useShiftNotification from "../../hooks/QueryHook/notification/shift-notificatoin/hook";
+import useLoanNotification from "../../hooks/QueryHook/notification/loan-notification/useLoanNotificaiton";
 import Card from "./components/card";
 
 const ParentNotification = () => {
@@ -10,6 +11,9 @@ const ParentNotification = () => {
   const { data: data2 } = useShiftNotification();
   const { data: data3 } = usePunchNotification();
   const { missPunchData } = useMissedPunchNotificationCount();
+  const { getEmployeeRequestLoanApplication } = useLoanNotification();
+
+  console.log("get pending loan" , getEmployeeRequestLoanApplication);
 
   const dummyData = [
     {
@@ -35,6 +39,12 @@ const ParentNotification = () => {
       count: missPunchData?.length ?? 0,
       color: "#51E8FD",
       url: "/missedPunch-notification",
+    },
+    {
+      name: "Loan Notification",
+      count: getEmployeeRequestLoanApplication?.length ?? 0,
+      color: "#51E8FD",
+      url: "/loan-notification",
     },
   ];
 
