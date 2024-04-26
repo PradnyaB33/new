b30517655f897b83749097eb54b7f05d61d0f834
Megha Dumbre <meghadu@ats.com>
2024-04-26 16:05:15 +0530

    AGS-476 Solved Bug

--- src/components/app-layout/components/test-nav-items.jsx
+++ src/components/app-layout/components/test-nav-items.jsx
@@ -265,7 +265,7 @@ const TestNavItems = ({ toggleDrawer }) => {
             ),
             link: `organisation/${orgId}/employee-onboarding`,
             icon: <PersonAdd className=" !text-[1.2em] text-[#67748E]" />,
-            text: "Employee Onboarding",
+            text: " Onboarding",
           },
 
           {
@@ -275,7 +275,7 @@ const TestNavItems = ({ toggleDrawer }) => {
             ),
             link: `organisation/${orgId}/employee-offboarding`,
             icon: <PersonRemove className=" !text-[1.2em] text-[#67748E]" />,
-            text: "Employee Offboarding",
+            text: " Offboarding",
           },
           {
             key: "employeeList",
