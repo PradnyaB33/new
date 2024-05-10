31bfd8310db86d432ca1199b57a19dd4470c7bf2
dev2.atsit <dev2.atsit@gmail.com>
2024-05-10 14:22:06 +0530

    fixed

--- src/pages/leave-notification/page.jsx
+++ src/pages/leave-notification/page.jsx
@@ -1,19 +1,13 @@
 import React from "react";
-import LeaveRejectmodal from "../../components/Modal/LeaveModal/LeaveRejectmodal";
 import useLeaveNotificationHook from "../../hooks/QueryHook/notification/leave-notification/hook";
+import LeaveAcceptModal from "./LeaveAcceptModal";
 
 const LeaveNotification = () => {
-  const { data, isLoading, isFetching } = useLeaveNotificationHook();
+  const { data } = useLeaveNotificationHook();
+  console.log("leave data", data);
   return (
-    <div className="pt-12 flex flex-col">
-      {data?.leaveRequests?.map((items, idx) => (
-        <LeaveRejectmodal
-          key={idx}
-          items={items}
-          isFetching={isFetching}
-          isLoading={isLoading}
-        />
-      ))}
+    <div className="flex flex-col">
+      <LeaveAcceptModal data={data} />
     </div>
   );
 };
