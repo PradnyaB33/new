c2fec8e9db6e9b625b1d1b11017b60ef1e630d32
dev2.atsit <dev2.atsit@gmail.com>
2024-05-11 10:03:30 +0530

    fixed

--- src/pages/Employee-Notification/components/card.jsx
+++ src/pages/Employee-Notification/components/card.jsx
@@ -18,17 +18,17 @@ const Card = ({ card, loading }) => {
     TrendingUp,
   ];
 
-  const Skeleton = () => {
-    return (
-      <div className="border-gray-200 border bg-white flex justify-between items-center shadow-lg sm:w-[300px] sm:pl-3 sm:pr-3 sm:h-[100px] h-[80px] w-full p-2 sm:m-6 gap-0 rounded-lg animate-pulse">
-        <div className="w-full">
-          <div className="h-5 bg-gray-300 mb-2 rounded-md"></div>
-          <div className="h-3 bg-gray-300 rounded-md"></div>
-        </div>
-        <div className="w-[40px] h-[40px] p-4 bg-gray-300 flex items-center justify-center rounded-lg"></div>
-      </div>
-    );
-  };
+  // const Skeleton = () => {
+  //   return (
+  //     <div className="border-gray-200 border bg-white flex justify-between items-center shadow-lg sm:w-[300px] sm:pl-3 sm:pr-3 sm:h-[100px] h-[80px] w-full p-2 sm:m-6 gap-0 rounded-lg animate-pulse">
+  //       <div className="w-full">
+  //         <div className="h-5 bg-gray-300 mb-2 rounded-md"></div>
+  //         <div className="h-3 bg-gray-300 rounded-md"></div>
+  //       </div>
+  //       <div className="w-[40px] h-[40px] p-4 bg-gray-300 flex items-center justify-center rounded-lg"></div>
+  //     </div>
+  //   );
+  // };
 
   return (
     <>
