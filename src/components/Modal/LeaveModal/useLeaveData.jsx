34ae8fccf555229a953f228c99f3b432a5d50be5
dev-2-atsit <dev2.atsit@gmail.com>
2024-05-19 23:23:06 +0530

    fixed

---
+++ src/components/Modal/LeaveModal/useLeaveData.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useMutation, useQueryClient } from "react-query";
+import useGetUser from "../../../hooks/Token/useUser";
+
+const useLeaveData = () => {
+  const { authToken } = useGetUser();
+  const queryClient = useQueryClient();
+  const { mutate: acceptDeleteLeaveMutation } = useMutation(
+    ({ id }) =>
+      axios.post(
+        `${process.env.REACT_APP_API}/route/leave/delete-request-accept/${id}`,
+        { message: "Your Request is successfully approved" },
+        {
+          headers: {
+            Authorization: authToken,
+          },
+        }
+      ),
+    {
+      onSuccess: async () => {
+        await queryClient?.invalidateQueries("employee-leave");
+        await queryClient?.invalidateQueries("EmpDataLeave");
+      },
+    }
+  );
+  const { mutate: rejectDeleteLeaveMutation } = useMutation(
+    ({ id }) =>
+      axios.post(
+        `${process.env.REACT_APP_API}/route/leave/delete-request-reject/${id}`,
+        { message: "Your Request is successfully approved" },
+        {
+          headers: {
+            Authorization: authToken,
+          },
+        }
+      ),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("employee-leave");
+        queryClient.invalidateQueries("EmpDataLeave");
+      },
+    }
+  );
+  return { acceptDeleteLeaveMutation, rejectDeleteLeaveMutation };
+};
+
+export default useLeaveData;
