3b12b0d5427148e6a5067a5e6d2fd1ac66959d4a
dev2.atsit <dev2.atsit@gmail.com>
2024-05-20 17:59:47 +0530

    fixed

--- src/pages/Billing/components/package/upgrade.jsx
+++ src/pages/Billing/components/package/upgrade.jsx
@@ -1,10 +1,12 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FactoryOutlined, Numbers } from "@mui/icons-material";
-import { Box, Button, Modal } from "@mui/material";
-import React from "react";
+import { Button } from "@mui/material";
+import moment from "moment";
+import React, { useCallback } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import AuthInputFiled from "../../../../components/InputFileds/AuthInputFiled";
+import ReusableModal from "../../../../components/Modal/component";
 import useManageSubscriptionMutation from "./subscription-mutaiton";
 const style = {
   position: "absolute",
@@ -17,6 +19,7 @@ const style = {
   overflow: "hidden",
 };
 const UpgradePackage = ({ handleClose, open, organisation }) => {
+  console.log(`🚀 ~ file: upgrade.jsx:22 ~ organisation:`, organisation);
   const [amount, setAmount] = React.useState(0);
   const { verifyPromoCodeMutation, handleUpgradeFunction } =
     useManageSubscriptionMutation();
@@ -38,50 +41,55 @@ const UpgradePackage = ({ handleClose, open, organisation }) => {
     discount: z.number().optional(),
   });
 
-  const { control, formState, handleSubmit, watch, setValue, getValues } =
-    useForm({
-      defaultValues: {
-        memberCount: `${organisation?.memberCount}` || "",
-        packageInfo: {
-          value: organisation?.packageInfo,
-          label: organisation?.packageInfo,
-          isDisabled: false,
-        },
-        paymentType: "RazorPay",
-        discount: 0,
+  const { control, formState, handleSubmit, watch, setValue } = useForm({
+    defaultValues: {
+      memberCount: `${organisation?.memberCount}` || "",
+      packageInfo: {
+        value: organisation?.packageInfo,
+        label: organisation?.packageInfo,
+        isDisabled: false,
       },
-      resolver: zodResolver(packageSchema),
-    });
-  console.log(getValues, setAmount);
+      paymentType: "RazorPay",
+      discount: 0,
+    },
+    resolver: zodResolver(packageSchema),
+  });
   const { errors, dirtyFields } = formState;
-
-  // const total = useMemo(async () => {
-  //   let tmAmount = await handleUpgradeFunction({
-  //     data: getValues(),
-  //     organisation,
-  //   });
-
-  //   tmAmount -= tmAmount * (watch("discount") / 100);
-  //   tmAmount = Math.round(tmAmount);
-  //   setAmount(() => tmAmount);
-  //   return tmAmount;
-  //   // eslint-disable-next-line
-  // }, [
-  //   watch("memberCount"),
-  //   watch("packageInfo.value"),
-  //   organisation,
-  //   getValues,
-  //   watch("discount"),
-  //   watch,
-  //   setAmount,
-  //   handleUpgradeFunction,
-  // ]);
 
   async function onSubmit(data) {
     data.totalAmount = amount;
     handleUpgradeFunction(data, organisation);
   }
 
+  const checkCallback = useCallback(() => {
+    // on package change update the per day value
+    let perDayValue = 0;
+    if (watch("packageInfo").value === "Basic Plan") {
+      perDayValue =
+        Number(process.env.REACT_APP_BASIC_PACKAGE_COST_DAY) /
+        moment().daysInMonth();
+    } else if (watch("packageInfo").value === "Intermediate Plan") {
+      perDayValue =
+        Number(process.env.REACT_APP_INTERMEDIATE_PACKAGE_COST_DAY) /
+        moment().daysInMonth();
+    } else {
+      perDayValue =
+        Number(process.env.REACT_APP_ENTERPRIZE_PACKAGE_COST_DAY) /
+        moment().daysInMonth();
+    }
+    return (
+      perDayValue *
+      moment(organisation?.subscriptionDetails?.expirationDate).diff(
+        moment(),
+        "days"
+      )
+    );
+  }, [
+    watch("packageInfo"),
+    watch("memberCount"),
+    watch("promoCode"),
+    watch("paymentType"),
+  ]);
   const checkDisability = () => {
     if (Object.keys(dirtyFields).length <= 1) {
       if (Object.keys(dirtyFields).includes("promoCode")) {
@@ -97,114 +105,95 @@ const UpgradePackage = ({ handleClose, open, organisation }) => {
     }
   };
   return (
-    <Modal
-      keepMounted={false}
+    <ReusableModal
+      heading={"Upgrade subscription"}
       open={open}
       onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
     >
-      <Box
-        sx={style}
-        className="border-none !z-10 shadow-md outline-none rounded-md flex flex-col relative"
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="overflow-auto h-full gap-4 flex-col flex"
+        noValidate
       >
-        <h1 className="p-4 text-xl font-semibold font-sans sticky top-0 bg-white z-10 shadow-inner border-b">
-          Upgrade subscription
-        </h1>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="overflow-auto h-full"
-          noValidate
-        >
-          <div className="flex flex-col gap-4 p-4 ">
-            <AuthInputFiled
-              name="memberCount"
-              icon={Numbers}
-              control={control}
-              type="number"
-              placeholder="Member Count "
-              label="Member Count *"
-              errors={errors}
-              error={errors.memberCount}
-            />
-            <AuthInputFiled
-              name="packageInfo"
-              icon={Numbers}
-              control={control}
-              type="select"
-              placeholder="Package Name "
-              label="Package Name *"
-              errors={errors}
-              error={errors.packageInfo}
-              options={[
-                { value: "Intermediate Plan", label: "Intermediate Plan" },
-                {
-                  value: "Basic Plan",
-                  label: "Basic Plan",
-                  isDisabled:
-                    organisation?.packageInfo === "Intermediate Plan"
-                      ? true
-                      : false,
-                },
-              ]}
-            />
+        <div className="flex flex-col">
+          <AuthInputFiled
+            name="memberCount"
+            icon={Numbers}
+            control={control}
+            type="number"
+            placeholder="Member Count "
+            label="Member Count *"
+            errors={errors}
+            error={errors.memberCount}
+          />
+          <AuthInputFiled
+            name="packageInfo"
+            icon={Numbers}
+            control={control}
+            type="select"
+            placeholder="Package Name "
+            label="Package Name *"
+            errors={errors}
+            error={errors.packageInfo}
+            options={[
+              { value: "Intermediate Plan", label: "Intermediate Plan" },
+              {
+                value: "Basic Plan",
+                label: "Basic Plan",
+                isDisabled:
+                  organisation?.packageInfo === "Intermediate Plan"
+                    ? true
+                    : false,
+              },
+            ]}
+          />
 
-            <AuthInputFiled
-              name="paymentType"
-              icon={FactoryOutlined}
-              control={control}
-              type="naresh-select"
-              placeholder="Select your Merchant"
-              label="Payment Gateway *"
-              errors={errors}
-              error={errors.paymentType}
-              options={[
-                { value: "Phone_Pay", label: "Phone_Pay" },
-                { value: "RazorPay", label: "RazorPay" },
-              ]}
-              descriptionText={"Additional 2% charges on razorpay transaction"}
-            />
-            <AuthInputFiled
-              name="promoCode"
-              icon={Numbers}
-              control={control}
-              type="input-action"
-              placeholder="#summer2021"
-              label="Promo Code"
-              errors={errors}
-              readOnly={watch("discount") > 0}
-              error={errors.promoCode}
-              descriptionText={
-                watch("discount")
-                  ? `You will get ${watch(
-                      "discount"
-                    )}% discount on your total amount.`
-                  : ""
-              }
-              onInputActionClick={(value) => {
-                verifyPromoCodeMutation({ promoCode: value, setValue });
-              }}
-              onInputActionClear={() => {
-                setValue("discount", 0);
-                setValue("promoCode", "");
-              }}
-            />
-          </div>
-          <div className="p-4 text-xl font-semibold font-sans sticky bottom-[-2px] bg-white z-10 shadow-inner border-b flex justify-between">
-            <Button variant="outlined" type="button" onClick={handleClose}>
-              Cancel
-            </Button>
-            <Button
-              variant="contained"
-              disabled={checkDisability()}
-              type="submit"
-            >
-              Pay {amount} Rs
-            </Button>
-          </div>
-        </form>
-      </Box>
-    </Modal>
+          <AuthInputFiled
+            name="paymentType"
+            icon={FactoryOutlined}
+            control={control}
+            type="naresh-select"
+            placeholder="Select your Merchant"
+            label="Payment Gateway *"
+            errors={errors}
+            error={errors.paymentType}
+            options={[
+              { value: "Phone_Pay", label: "Phone_Pay" },
+              { value: "RazorPay", label: "RazorPay" },
+            ]}
+            descriptionText={"Additional 2% charges on razorpay transaction"}
+          />
+          <AuthInputFiled
+            name="promoCode"
+            icon={Numbers}
+            control={control}
+            type="input-action"
+            placeholder="#summer2021"
+            label="Promo Code"
+            errors={errors}
+            readOnly={watch("discount") > 0}
+            error={errors.promoCode}
+            descriptionText={
+              watch("discount")
+                ? `You will get ${watch(
+                    "discount"
+                  )}% discount on your total amount.`
+                : ""
+            }
+            onInputActionClick={(value) => {
+              verifyPromoCodeMutation({ promoCode: value, setValue });
+            }}
+            onInputActionClear={() => {
+              setValue("discount", 0);
+              setValue("promoCode", "");
+            }}
+          />
+        </div>
+        <Button variant="contained" disabled={checkDisability()} type="submit">
+          Pay {Math.round(checkCallback())} Rs
+        </Button>
+      </form>
+    </ReusableModal>
   );
 };
 
