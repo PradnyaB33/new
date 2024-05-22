2d47a25af7dd8bd3ea5ff18e17f6784c8bed4847
Dev4 <dev4.atsit@gmail.com>
2024-05-22 16:25:59 +0530

    mis pf challan completed

--- src/pages/DashBoard/Components/Card/superadmin/SuperAdminCard.jsx
+++ src/pages/DashBoard/Components/Card/superadmin/SuperAdminCard.jsx
@@ -15,7 +15,7 @@ const SuperAdminCard = ({
       {isLoading ? (
         <AdminCardSke />
       ) : (
-        <div className="hover:scale-105 !px-0 !py-0  h-max transition-all w-full flex-1      shadow-md  bg-white !rounded-xl ">
+        <div className="hover:scale-105 !px-0 !py-0  h-max transition-all w-full flex-1 border bg-white !rounded-md ">
           <div className="space-y-2 w-full !px-6 !py-2  flex justify-between">
             <div className="w-[50%]">
               <h1 className="md:text-md text-sm font-bold text-[#67748E]  ">
