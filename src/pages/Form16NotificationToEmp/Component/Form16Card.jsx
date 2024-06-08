c9724c0a3daff85813b8c9d3ccab1a52f93fe640
Megha Dumbre <meghadu@ats.com>
2024-06-08 11:07:21 +0530

    AGS-699 FORM 16 NOTIFICATION

--- src/pages/Form16NotificationToEmp/Component/Form16Card.jsx
+++ src/pages/Form16NotificationToEmp/Component/Form16Card.jsx
@@ -2,6 +2,7 @@ import { Grid, Box } from "@mui/material";
 import React from "react";
 
 const Form16Card = ({ items }) => {
+  console.log("items", items);
   return (
     <Box
       className="py-2 space-y-5 h-max"
@@ -21,13 +22,13 @@ const Form16Card = ({ items }) => {
         <Grid item className="gap-1 py-4 w-full h-max space-y-4">
           <Box className="flex md:flex-row items-center justify-center flex-col gap-8 md:gap-16">
             <div className="space-y-4 w-full flex flex-col items-center md:items-start justify-center">
-              {items && items.length > 0 ? (
+              {items ? (
                 <h1 className="text-xl px-4 md:!px-0 font-semibold">
-                  Form 16 is uploaded for {items[0].year} year.
+                  Form 16 is uploaded for {items.year} year.
                 </h1>
               ) : (
                 <h1 className="text-xl px-4 md:!px-0 font-semibold">
-                 Sorry, no request found.
+                  Sorry, no request found.
                 </h1>
               )}
             </div>
--- src/pages/Form16NotificationToEmp/Component/InputForm.jsx
+++ src/pages/Form16NotificationToEmp/Component/InputForm.jsx
@@ -3,16 +3,18 @@ import Form16Card from "./Form16Card";
 import useForm16NotificationHook from "../../../hooks/QueryHook/notification/Form16Notification/useForm16NotificationHook";
 
 const InputForm = () => {
-  const { Form16Notification } =
-    useForm16NotificationHook();
+  const { Form16Notification } = useForm16NotificationHook();
   console.log("form16notification", Form16Notification);
 
   return (
     <div className="flex w-full flex-col gap-4">
-      {Form16Notification?.map((item, index) => (
-        <Form16Card key={index} items={item} />
-      ))}
-      {!Form16Notification && <h1>Sorry, no request found</h1>}
+      {Form16Notification && Form16Notification.length > 0 ? (
+        Form16Notification.map((item, index) => (
+          <Form16Card key={index} items={item} />
+        ))
+      ) : (
+        <h1>Sorry, no request found</h1>
+      )}
     </div>
   );
 };
