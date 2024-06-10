c909a3d50076f81bbdddd8f55432be7b9ebabf7d
dev2.atsit <dev2.atsit@gmail.com>
2024-06-10 14:32:20 +0530

    fixed

--- src/pages/AddOrganisation/components/step-4.jsx
+++ src/pages/AddOrganisation/components/step-4.jsx
@@ -111,9 +111,9 @@ const Step4 = () => {
       );
     },
   });
-  const { mutate: mutate2, isLoading: isLoading2 } = useMutation({
-    mutationFn: handleDismiss,
-  });
+  // const { mutate: mutate2, isLoading: isLoading2 } = useMutation({
+  //   mutationFn: handleDismiss,
+  // });
   console.log(
     `🚀 ~ file: step-4.jsx:124 ~ data?.packageInfo:`,
     data?.packageInfo
@@ -138,9 +138,9 @@ const Step4 = () => {
   if (isLoading) {
     return <Loader />;
   }
-  if (isLoading2) {
-    return <Loader />;
-  }
+  // if (isLoading2) {
+  //   return <Loader />;
+  // }
 
   return (
     <div className="px-4 grid bg-[#f8fafb] p-4 rounded-md items-center">
