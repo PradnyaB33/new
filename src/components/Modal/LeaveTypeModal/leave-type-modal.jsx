985b53a99cc1758228e6ec0bf056287d97a2e14c
dev-2-atsit <dev2.atsit@gmail.com>
2024-06-15 17:29:59 +0530

    fixed

--- src/components/Modal/LeaveTypeModal/leave-type-modal.jsx
+++ src/components/Modal/LeaveTypeModal/leave-type-modal.jsx
@@ -74,15 +74,6 @@ const LeaveTypeModal = ({ handleClose, open, id, leaveType }) => {
     }
   };
 
-  const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    bgcolor: "background.paper",
-    p: 4,
-  };
-
   return (
     // <Modal
     //   open={open}
