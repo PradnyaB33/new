1c0e1783b59cc14b3d920a372512cfdaa6898470
dev-2-atsit <dev2.atsit@gmail.com>
2024-06-15 17:13:25 +0530

    fixed

--- src/components/Modal/LeaveTypeModal/create-leve-type-modal.jsx
+++ src/components/Modal/LeaveTypeModal/create-leve-type-modal.jsx
@@ -1,13 +1,10 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Add, Close, ToggleOn, WorkOffOutlined } from "@mui/icons-material";
+import { Add, ToggleOn, WorkOffOutlined } from "@mui/icons-material";
 import {
-  Box,
   Button,
   CircularProgress,
   FormControl,
   FormLabel,
-  IconButton,
-  Modal,
   Stack,
 } from "@mui/material";
 import axios from "axios";
@@ -20,6 +17,7 @@ import { z } from "zod";
 import { TestContext } from "../../../State/Function/Main";
 import { UseContext } from "../../../State/UseState/UseContext";
 import AuthInputFiled from "../../InputFileds/AuthInputFiled";
+import ReusableModal from "../component";
 
 const CreteLeaveTypeModal = ({ handleClose, open }) => {
   const { handleAlert } = useContext(TestContext);
@@ -105,107 +103,90 @@ const CreteLeaveTypeModal = ({ handleClose, open }) => {
   };
 
   return (
-    <Modal
-      keepMounted={false}
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box
-        sx={style}
-        className="border-none !z-10 shadow-md outline-none rounded-md gap-6 flex flex-col"
-      >
-        <h1 className="text-xl font-semibold font-sans text-center pb-2 border-b-2 flex justify-between items-baseline">
-          Add leave type
-          <IconButton onClick={handleClose}>
-            <Close />
-          </IconButton>
-        </h1>
-        <form onSubmit={handleSubmit(onSubmit)} noValidate>
-          <Stack width={400}>
-            <AuthInputFiled
-              name="leaveName"
-              icon={WorkOffOutlined}
-              control={control}
-              type="text"
-              placeholder="eg. Sick leave"
-              label="Leave Type Name *"
-              errors={errors}
-              error={errors.leaveName}
-            />
-            <AuthInputFiled
-              name="count"
-              icon={Add}
-              control={control}
-              type="number"
-              placeholder="eg. 4"
-              label="Enter Count *"
-              errors={errors}
-              error={errors.count}
-            />
-            <FormControl component="fieldset">
-              <FormLabel
-                component="legend"
-                className="!font-semibold !text-gray-500 !text-md"
-              >
-                Color
-              </FormLabel>
-              <Controller
-                name="color"
-                control={control}
-                render={({ field }) => {
-                  return (
-                    <div
-                      className="rounded-full overflow-hidden relative"
-                      style={{
-                        height: "40px",
-                        width: "40px",
-                      }}
-                    >
-                      <input
-                        value={field.value}
-                        required
-                        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                        style={{
-                          height: "60px",
-                          width: "60px",
-                          padding: "0",
-                          border: "none",
-                        }}
-                        type="color"
-                        id="favcolor"
-                        onChange={(e) => field.onChange(e.target.value)}
-                      />
-                    </div>
-                  );
-                }}
-              />
-            </FormControl>
-            <AuthInputFiled
-              name="isActive"
-              icon={ToggleOn}
-              control={control}
-              type="checkbox"
-              placeholder="eg. 4"
-              label="Is Active *"
-              errors={errors}
-              error={errors.count}
-            />
-            <Button
-              disabled={isFormClean || isLoading}
-              type="submit"
-              variant="contained"
+    <ReusableModal heading={"Add leave type"} open={open} onClose={handleClose}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <Stack width={400}>
+          <AuthInputFiled
+            name="leaveName"
+            icon={WorkOffOutlined}
+            control={control}
+            type="text"
+            placeholder="eg. Sick leave"
+            label="Leave Type Name *"
+            errors={errors}
+            error={errors.leaveName}
+          />
+          <AuthInputFiled
+            name="count"
+            icon={Add}
+            control={control}
+            type="number"
+            placeholder="eg. 4"
+            label="Enter Count *"
+            errors={errors}
+            error={errors.count}
+          />
+          <FormControl component="fieldset">
+            <FormLabel
+              component="legend"
+              className="!font-semibold !text-gray-500 !text-md"
             >
-              <div className="w-6 h-6">
-                {isLoading && <CircularProgress size={20} />}
-              </div>
-              Submit
-            </Button>
-          </Stack>
-        </form>
-      </Box>
-    </Modal>
+              Color
+            </FormLabel>
+            <Controller
+              name="color"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <div
+                    className="rounded-full overflow-hidden relative"
+                    style={{
+                      height: "40px",
+                      width: "40px",
+                    }}
+                  >
+                    <input
+                      value={field.value}
+                      required
+                      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                      style={{
+                        height: "60px",
+                        width: "60px",
+                        padding: "0",
+                        border: "none",
+                      }}
+                      type="color"
+                      id="favcolor"
+                      onChange={(e) => field.onChange(e.target.value)}
+                    />
+                  </div>
+                );
+              }}
+            />
+          </FormControl>
+          <AuthInputFiled
+            name="isActive"
+            icon={ToggleOn}
+            control={control}
+            type="checkbox"
+            placeholder="eg. 4"
+            label="Is Active *"
+            errors={errors}
+            error={errors.count}
+          />
+          <Button
+            disabled={isFormClean || isLoading}
+            type="submit"
+            variant="contained"
+          >
+            <div className="w-6 h-6">
+              {isLoading && <CircularProgress size={20} />}
+            </div>
+            Submit
+          </Button>
+        </Stack>
+      </form>
+    </ReusableModal>
   );
 };
 
