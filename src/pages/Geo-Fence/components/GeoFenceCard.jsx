f5215d0295662b16af9bfe97ad82baf4f312fc15
dev-2-atsit <dev2.atsit@gmail.com>
2024-06-25 07:32:41 +0530

    added page for super admin and manager

--- src/pages/Geo-Fence/components/GeoFenceCard.jsx
+++ src/pages/Geo-Fence/components/GeoFenceCard.jsx
@@ -1,21 +1,29 @@
 import { MoreVert, MyLocation } from "@mui/icons-material";
-import { Button } from "@mui/material";
-import React from "react";
+import { Button, IconButton, Menu, MenuItem } from "@mui/material";
+import React, { useState } from "react";
 
 const GeoFenceCard = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="bg-white rounded-lg shadow-lg">
       <div className="flex flex-col justify-start items-center">
         <div className="flex p-4 justify-between gap-8 border-b w-full">
-          <div className="flex gap-2">
+          <div className="flex gap-2 items-center">
             <MyLocation className="text-Brand-washed-blue/brand-washed-blue-10" />
             <h4 className="text-xl underline text-black">Mumbai,Pune</h4>
           </div>
-          <MoreVert />
+          <IconButton onClick={() => setOpen(true)}>
+            <MoreVert />
+          </IconButton>
+          <Menu open={open}>
+            <MenuItem>Delete</MenuItem>
+          </Menu>
         </div>
-        <div className="p-4 w-full">
-          <p className="text-xl w-full text-black">Employee Count: 08</p>
-          <p className="text-sm w-full">Employee Count: 08</p>
+        <div className="p-4 w-full flex flex-col gap-4">
+          <div className="">
+            <p className="text-xl w-full text-black">Employee Count: 08</p>
+            <p className="text-sm w-full">Employee Count: 08</p>
+          </div>
           <div className="flex gap-6 justify-between">
             <Button variant="contained" size="small">
               Add Employee
