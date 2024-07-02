51b3668aeff3a280f6565c15996d01f1f5391069
dev-2-atsit <dev2.atsit@gmail.com>
2024-07-02 08:02:38 +0530

    fixed

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -11,6 +11,7 @@ import { z } from "zod";
 import { TestContext } from "../../State/Function/Main";
 import { UseContext } from "../../State/UseState/UseContext";
 import AuthInputFiled from "../../components/InputFileds/AuthInputFiled";
+import useLoadModel from "../../hooks/FaceMode/useFaceModal";
 import UserProfile from "../../hooks/UserData/useUser";
 import useHook from "../../hooks/UserProfile/useHook";
 import { getSignedUrl, uploadFile } from "../../services/api";
@@ -23,11 +24,12 @@ const EmployeeProfile = () => {
   const user = getCurrentUser();
   const userId = user._id;
   const queryClient = useQueryClient();
-  const [error, setError] = useState();
   const { UserInformation } = useHook();
   const [url, setUrl] = useState();
   const fileInputRef = useRef();
   const [file, setFile] = useState();
+  const { data, detectFacesMutation } = useLoadModel();
+  console.log(`🚀 ~ file: UserProfile.jsx:32 ~ data:`, data);
 
   const UserProfileSchema = z.object({
     additional_phone_number: z
@@ -79,20 +81,25 @@ const EmployeeProfile = () => {
         );
         setValue("status_message", data?.employee?.status_message);
       },
-      onError: () => {
-        setError("Error fetching profile data.");
-      },
+      onError: () => {},
     }
   );
-  console.log({profileData , isLoading});
+  console.log({ profileData, isLoading });
 
   const handleImageChange = (e) => {
     const selectedFile = e.target.files[0];
     if (selectedFile && selectedFile.type.startsWith("image/")) {
       setFile(selectedFile);
       const reader = new FileReader();
-      reader.onloadend = () => {
+      reader.onloadend = async () => {
+        console.log(`🚀 ~ file: UserProfile.jsx:94 ~ reader:`, reader);
         setUrl(reader.result);
+        const canvas = document.getElementById("canvas-1");
+        const ctx = canvas.getContext("2d");
+        const img = document.getElementById("image-1");
+        ctx.drawImage(img, 0, 0, 300, 300);
+        const faces = await detectFacesMutation({ img, canvasId: "canvas-1" });
+        console.log(`🚀 ~ file: UserProfile.jsx:102 ~ faces:`, faces);
       };
       reader.readAsDataURL(selectedFile);
     } else {
@@ -117,9 +124,7 @@ const EmployeeProfile = () => {
         handleAlert(true, "success", "Additional details added successfully!");
         reset();
       },
-      onError: () => {
-        setError("Error updating additional details.");
-      },
+      onError: () => {},
     }
   );
 
@@ -141,11 +146,8 @@ const EmployeeProfile = () => {
     } catch (error) {
       console.error(error);
       handleAlert(true, "error", "Error updating additional details");
-      setError("Error updating additional details");
     }
   };
-
-  console.log(error);
 
   return (
     <div>
@@ -179,15 +181,24 @@ const EmployeeProfile = () => {
                 />
                 <div className="w-full h-full flex flex-col justify-center items-center">
                   {url || UserInformation?.user_logo_url ? (
-                    <img
-                      src={url || UserInformation?.user_logo_url}
-                      alt="Selected"
-                      style={{
-                        width: "150px",
-                        height: "150px",
-                        borderRadius: "50%",
-                      }}
-                    />
+                    <div className="relative">
+                      <img
+                        id="image-1"
+                        src={url || UserInformation?.user_logo_url}
+                        alt="Selected"
+                        style={{
+                          width: "150px",
+                          height: "150px",
+                          borderRadius: "50%",
+                          display: "none",
+                        }}
+                        crossOrigin="anonymous"
+                      />
+                      <canvas
+                        id="canvas-1"
+                        className="bg-gray-50 w-[150px] h-[150px] rounded-full"
+                      />
+                    </div>
                   ) : (
                     <Skeleton variant="circular" width="150px" height="150px" />
                   )}
