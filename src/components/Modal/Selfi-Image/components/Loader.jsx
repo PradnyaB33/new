a2b291c9f4aac90da9066b42973224775c6e832c
dev2.atsit <dev2.atsit@gmail.com>
2024-07-06 17:48:12 +0530

    fixed

--- src/components/Modal/Selfi-Image/components/Loader.jsx
+++ src/components/Modal/Selfi-Image/components/Loader.jsx
@@ -4,9 +4,13 @@ import React from "react";
 const Loader = ({ isLoading = false }) => {
   return (
     isLoading && (
-      <div className="preloader12 absolute left-0 right-0 m-auto inset-0 flex items-center justify-center p">
-        <EmojiEmotionsOutlined className="pulse text-white !text-4xl" />
-      </div>
+      <>
+        {" "}
+        <div className="preloader12 absolute left-0 right-0 m-auto inset-0 flex items-center justify-center p">
+          <EmojiEmotionsOutlined className="pulse text-white !text-4xl" />
+        </div>
+        <p>Recognizing face and authenticating</p>
+      </>
     )
   );
 };
