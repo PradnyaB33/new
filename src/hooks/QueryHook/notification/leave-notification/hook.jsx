cd3c2efa71d3ce614b9b324b68fb71cfd0e06a51
dev2.atsit <dev2.atsit@gmail.com>
2024-07-15 16:21:09 +0530

    small changes

--- src/hooks/QueryHook/notification/leave-notification/hook.jsx
+++ src/hooks/QueryHook/notification/leave-notification/hook.jsx
@@ -1,14 +1,22 @@
 import axios from "axios";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import useNotificationCount from "../../../../components/app-layout/notification-zustand";
 import useGetUser from "../../../Token/useUser";
 
 const useLeaveNotificationHook = () => {
-  const { authToken } = useGetUser();
+  const { authToken, decodedToken } = useGetUser();
+  console.log(`🚀 ~ file: hook.jsx:8 ~ decodedToken:`, decodedToken);
   const { setNotificationCount } = useNotificationCount();
+  const [organizationId, setOrganizationId] = useState(
+    decodedToken?.user?.organizationId
+  );
+  const updateOrganizationId = (orgId) => {
+    setOrganizationId((prev) => orgId);
+  };
   const getUserNotification = async () => {
     const response = await axios.get(
-      `${process.env.REACT_APP_API}/route/leave/get`,
+      `${process.env.REACT_APP_API}/route/leave/get?organizationId=${organizationId}`,
       {
         headers: { Authorization: authToken },
       }
@@ -16,7 +24,7 @@ const useLeaveNotificationHook = () => {
     return response.data;
   };
   const { data, isLoading, isFetching } = useQuery(
-    "employee-leave",
+    ["employee-leave", organizationId],
     getUserNotification,
     {
       onSuccess: async (data) => {
@@ -30,6 +38,8 @@ const useLeaveNotificationHook = () => {
     data,
     isLoading,
     isFetching,
+    updateOrganizationId,
+    organizationId,
   };
 };
 
