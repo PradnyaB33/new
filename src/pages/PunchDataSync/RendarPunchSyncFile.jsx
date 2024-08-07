8ae738d1f1f49dac24bf700640781df7a54f644d
Megha Dumbre <meghadu@ats.com>
2024-08-07 17:38:17 +0530

    AGS-505 Punching attendance

--- src/pages/PunchDataSync/RendarPunchSyncFile.jsx
+++ src/pages/PunchDataSync/RendarPunchSyncFile.jsx
@@ -1,56 +1,3 @@
-// import { Container } from "@mui/material";
-// import React, { useContext } from "react";
-// import { useParams } from "react-router-dom";
-// import { useQuery } from "react-query";
-// import axios from "axios";
-// import EmpInfoByDynimacally from "./EmpInfoByDynanimacally";
-// import EmpInfoPunchStatus from "./EmpInfoPunchStatus";
-// import { UseContext } from "../../State/UseState/UseContext";
-
-// const RenderPunchSyncFile = () => {
-//   const { organisationId } = useParams();
-//   // Get cookies
-//   const { cookies } = useContext(UseContext);
-//   const authToken = cookies["aegis"];
-
-//   // Get punching data by org
-//   const { data: tempPunchData } = useQuery(
-//     ["tempPunchData", organisationId],
-//     async () => {
-//       const response = await axios.get(
-//         `${process.env.REACT_APP_API}/route/organization/${organisationId}/get-temp-punching-data`,
-//         {
-//           headers: {
-//             Authorization: authToken,
-//           },
-//         }
-//       );
-//       return response.data;
-//     },
-//     {
-//       enabled: !!organisationId && !!authToken,
-//     }
-//   );
-
-//   console.log("tempPunchData", tempPunchData);
-
-//   // Determine which component to render based on the role
-//   const renderPunchSyncComponent = () => {
-//     if (!tempPunchData || tempPunchData.length === 0) {
-//       return <EmpInfoPunchStatus organisationId={organisationId} />;
-//     } else {
-//       return <EmpInfoByDynimacally organisationId={organisationId} />;
-//     }
-//   };
-
-//   return (
-//     <Container maxWidth="xl" className="bg-gray-50 min-h-screen">
-//       {renderPunchSyncComponent()}
-//     </Container>
-//   );
-// };
-
-// export default RenderPunchSyncFile;
 import React, { useContext, useState } from "react";
 import { Container, Button, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
@@ -64,7 +11,7 @@ const RenderPunchSyncFile = () => {
   const { organisationId } = useParams();
   const { cookies } = useContext(UseContext);
   const authToken = cookies["aegis"];
-  const [syncOption, setSyncOption] = useState("file"); // Default is "file"
+  const [syncOption, setSyncOption] = useState("file");
 
   // Get punching data by org
   const { data: tempPunchData } = useQuery(
@@ -94,10 +41,10 @@ const RenderPunchSyncFile = () => {
 
   return (
     <Container maxWidth="xl" className="bg-gray-50 min-h-screen">
-      <Typography variant="h5" className="mb-4">
+      <Typography variant="h5" className="mb-4" style={{ padding: "10px 0" }}>
         Do you want to sync the data via:
       </Typography>
-      <div className="mb-6">
+      <div className="mb-6" style={{ display: "flex", gap: "16px" }}>
         <Button
           variant={syncOption === "file" ? "contained" : "outlined"}
           onClick={() => handleSyncOptionChange("file")}
@@ -107,7 +54,6 @@ const RenderPunchSyncFile = () => {
         <Button
           variant={syncOption === "dynamic" ? "contained" : "outlined"}
           onClick={() => handleSyncOptionChange("dynamic")}
-          className="ml-4"
         >
           Dynamically Display
         </Button>
