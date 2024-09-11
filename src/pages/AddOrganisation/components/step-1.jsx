4c174225b99afd58a991c9b5d9a766786794a60f
dev2 <dev2.atsit@gmail.com>
2024-09-11 15:47:43 +0530

    Modified Add Organisation

--- src/pages/AddOrganisation/components/step-1.jsx
+++ src/pages/AddOrganisation/components/step-1.jsx
@@ -1,4 +1,3 @@
-import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Business,
   CalendarMonthOutlined,
@@ -8,22 +7,21 @@ import {
   LocalPostOfficeOutlined,
   LocationOn,
   Phone,
+
   TodayOutlined,
 } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import { FaLinkedin } from "react-icons/fa";
 import React from "react";
 import { useForm } from "react-hook-form";
-import { FaLinkedin } from "react-icons/fa";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import useOrg from "../../../State/Org/Org";
 import AuthInputFiled from "../../../components/InputFileds/AuthInputFiled";
+import useOrg from "../../../State/Org/Org";
 import useGetUser from "../../../hooks/Token/useUser";
 
-// to define the schema for organization validation
 const organizationSchema = z.object({
-  orgName: z
-    .string()
-    .max(32, { message: "Name must be at least 32 characters" }),
+  orgName: z.string().max(32, { message: "Name must be at least 32 characters" }),
   foundation_date: z.string().refine(
     (date) => {
       const currentDate = new Date().toISOString().split("T")[0];
@@ -31,135 +29,84 @@ const organizationSchema = z.object({
     },
     { message: "Foundation date must be less than or equal to current date" }
   ),
-  web_url: z.string(),
-  industry_type: z.enum(["Technology", "Finance", "Healthcare", "Education"]),
+  web_url: z.string().optional(),
+  industry_type: z.string().refine(
+    (val) => {
+      const predefinedValues = [
+        "Technology", "Finance", "Healthcare", "Education", "Manufacturing",
+        "Retail", "Transportation", "Telecommunications", "Real Estate",
+        "Hospitality", "Pharmaceuticals", "Automotive", "Insurance",
+        "Nonprofit", "Government", "Consulting", "Media", "Advertising",
+        "Biotechnology"
+      ];
+      return predefinedValues.includes(val) || val === "other";
+    },
+    { message: "Invalid industry type" }
+  ),
+  custom_industry_type: z.string().optional(),
   email: z.string().email(),
-  organization_linkedin_url: z.string(),
-  location: z
-    .any({
-      address: z.string(),
-      position: z.object({
-        lat: z.number(),
-        lng: z.number(),
-      }),
-    })
-    .refine(
-      (val) => {
-        return (
-          val.address !== ("" || undefined) &&
-          val.position.lat !== 0 &&
-          val.position.lng !== 0
-        );
-      },
-      { message: "Location is required" }
-    ),
-  contact_number: z
-    .string()
-    .max(10, { message: "contact number must be 10 digits" })
-    .min(10, { message: "contact number must be 10 digits" }),
-  description: z.string(),
-  creator: z.string(),
-
+  organization_linkedin_url: z.string().optional(),
+  location: z.any().refine(
+    (val) => {
+      return (
+        val.address !== ("" || undefined) &&
+        val.position.lat !== 0 &&
+        val.position.lng !== 0
+      );
+    },
+    { message: "Location is required" }
+  ),
+  contact_number: z.string().length(10, { message: "Contact number must be 10 digits" }),
+  description: z.string().optional(),
+  creator: z.string().optional(),
+  gst_number: z.string().optional(),
   isTrial: z.boolean(),
 });
+
 const Step1 = ({ nextStep }) => {
-  // to state, hook , import other funciton
+   // to state, hook , import other funciton
   const { decodedToken } = useGetUser();
   const {
-    orgName,
-    foundation_date,
-    web_url,
-    industry_type,
-    email,
-    organization_linkedin_url,
-    location,
-    contact_number,
-    description,
-    setStep1Data,
-    isTrial,
+    orgName, foundation_date, web_url, industry_type, email,
+    organization_linkedin_url, location, contact_number, description,
+    setStep1Data, isTrial
   } = useOrg();
- 
-  // use useForm
+ // use useForm
   const { control, formState, handleSubmit, watch } = useForm({
     defaultValues: {
-      orgName: orgName,
-      foundation_date: foundation_date,
-      web_url: web_url,
-      industry_type: industry_type,
-      email: email,
-      organization_linkedin_url: organization_linkedin_url,
-      location: location,
-      contact_number: contact_number,
-      description: description,
-      creator: decodedToken?.user?._id,
-      isTrial: isTrial,
+      orgName, foundation_date, web_url, industry_type, email,
+      organization_linkedin_url, location, contact_number, description,
+      creator: decodedToken?.user?._id, gst_number: "", isTrial
     },
     resolver: zodResolver(organizationSchema),
   });
+
   const { errors } = formState;
 
-//  define the onSubmit function
   const onSubmit = async (data) => {
+ 
+    if (data.industry_type === "other") {
+      data.industry_type = data.custom_industry_type;
+    }
     await setStep1Data(data);
     nextStep();
   };
 
   return (
     <div>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="item-center flex flex-col"
-        noValidate
-      >
+      <form onSubmit={handleSubmit(onSubmit)} className="item-center flex flex-col" noValidate>
         <div className="grid md:grid-cols-2 md:gap-4 gap-0 px-4 grid-cols-1">
-          <AuthInputFiled
-            name="orgName"
-            icon={Business}
-            control={control}
-            type="text"
-            placeholder="Organisation Name"
-            label="Organisation Name *"
-            errors={errors}
-            error={errors.orgName}
-          />
-          <AuthInputFiled
-            name="foundation_date"
-            icon={TodayOutlined}
-            control={control}
-            type="date"
-            placeholder="Foundation Date"
-            label="Foundation Date *"
-            max={new Date().toISOString().split("T")[0]}
-            errors={errors}
-            error={errors.foundation_date}
-          />
-          <AuthInputFiled
-            name="web_url"
-            icon={Link}
-            control={control}
-            type="text"
-            placeholder="Web URL "
-            label="Web URL  *"
-            errors={errors}
-            error={errors.web_url}
-          />
-          <AuthInputFiled
-            name="organization_linkedin_url"
-            icon={FaLinkedin}
-            control={control}
-            type="text"
-            placeholder="LinkedIn URL "
-            label="LinkedIn URL  *"
-            errors={errors}
-            error={errors.organization_linkedin_url}
-          />
+          <AuthInputFiled name="orgName" icon={Business} control={control} type="text" placeholder="Organisation Name" label="Organisation Name *" errors={errors} error={errors.orgName} />
+          <AuthInputFiled name="foundation_date" icon={TodayOutlined} control={control} type="date" placeholder="Foundation Date" label="Foundation Date *" max={new Date().toISOString().split("T")[0]} errors={errors} error={errors.foundation_date} />
+          <AuthInputFiled name="web_url" icon={Link} control={control} type="text" placeholder="Web URL" label="Web URL" errors={errors} error={errors.web_url} />
+          <AuthInputFiled name="organization_linkedin_url" icon={FaLinkedin} control={control} type="text" placeholder="LinkedIn URL" label="LinkedIn URL" errors={errors} error={errors.organization_linkedin_url} />
           <AuthInputFiled
             name="industry_type"
             icon={FactoryOutlined}
             control={control}
             type="naresh-select"
-            placeholder="Type of Industry "
-            label="Type of Industry  *"
+            placeholder="Type of Industry"
+            label="Type of Industry "
             errors={errors}
             error={errors.industry_type}
             options={[
@@ -167,60 +114,43 @@ const Step1 = ({ nextStep }) => {
               { value: "Finance", label: "Finance" },
               { value: "Healthcare", label: "Healthcare" },
               { value: "Education", label: "Education" },
+              { value: "Manufacturing", label: "Manufacturing" },
+              { value: "Retail", label: "Retail" },
+              { value: "Transportation", label: "Transportation" },
+              { value: "Telecommunications", label: "Telecommunications" },
+              { value: "Real Estate", label: "Real Estate" },
+              { value: "Hospitality", label: "Hospitality" },
+              { value: "Pharmaceuticals", label: "Pharmaceuticals" },
+              { value: "Automotive", label: "Automotive" },
+              { value: "Insurance", label: "Insurance" },
+              { value: "Nonprofit", label: "Nonprofit" },
+              { value: "Government", label: "Government" },
+              { value: "Consulting", label: "Consulting" },
+              { value: "Media", label: "Media" },
+              { value: "Advertising", label: "Advertising" },
+              { value: "Biotechnology", label: "Biotechnology" },
+              { value: "other", label: "Other" }
             ]}
           />
-          <AuthInputFiled
-            name="email"
-            icon={LocalPostOfficeOutlined}
-            control={control}
-            type="email"
-            placeholder="Organisation Email "
-            label="Organisation Email  *"
-            errors={errors}
-            error={errors.email}
-          />
-          <AuthInputFiled
-            name="contact_number"
-            icon={Phone}
-            control={control}
-            type="number"
-            placeholder="Contact Number "
-            label="Contact Number  *"
-            errors={errors}
-            error={errors.contact_number}
-          />
-          <AuthInputFiled
-            name="description"
-            icon={Description}
-            control={control}
-            type="text"
-            placeholder="Organisational Description "
-            label="Organisational Description  *"
-            errors={errors}
-            error={errors.description}
-          />
-          <AuthInputFiled
-            className="w-full"
-            name="location"
-            icon={LocationOn}
-            control={control}
-            placeholder="eg. Kathmandu, Nepal"
-            type="location-picker"
-            label="Location *"
-            errors={errors}
-            error={errors.location}
-            value={watch("location")}
-          />
-          <div className=" mt-7">
+          {watch("industry_type") === "other" && (
             <AuthInputFiled
-              name="isTrial"
-              icon={CalendarMonthOutlined}
+              name="custom_industry_type"
+              icon={FactoryOutlined}
               control={control}
-              type="checkbox"
-              label="Do you want 7 day Trial"
+              type="text"
+              placeholder="Specify Custom Industry"
+              label="Specify Custom Industry"
               errors={errors}
-              error={errors.location}
+              error={errors.custom_industry_type}
             />
+          )}
+          <AuthInputFiled name="email" icon={LocalPostOfficeOutlined} control={control} type="email" placeholder="Organisation Email" label="Organisation Email *" errors={errors} error={errors.email} />
+          <AuthInputFiled name="contact_number" icon={Phone} control={control} type="number" placeholder="Contact Number" label="Contact Number *" errors={errors} error={errors.contact_number} />
+          <AuthInputFiled name="description" icon={Description} control={control} type="text" placeholder="Organisational Description" label="Organisational Description" errors={errors} error={errors.description} />
+          <AuthInputFiled className="w-full" name="location" icon={LocationOn} control={control} placeholder="eg. Kathmandu, Nepal" type="location-picker" label="Location *" errors={errors} error={errors.location} value={watch("location")} />
+          <AuthInputFiled name="gst_number" icon={Description} control={control} type="text" placeholder="GST Number" label="GST Number" errors={errors} error={errors.gst_number} />
+          <div className="mt-7">
+            <AuthInputFiled name="isTrial" icon={CalendarMonthOutlined} control={control} type="checkbox" label="Do you want 7 day Trial" errors={errors} error={errors.isTrial} />
           </div>
         </div>
         <Button type="submit" variant="contained" className="!w-max !mx-auto">
