ad2f226cf173b9159a53ed221d1b2ea6edeb4405
Mayuri Jugdar <mayuriju@ats.com>
2024-09-16 18:29:14 +0530

    resolved bug

--- src/pages/Income/components/accountantDeclarations/DeclarationPage.jsx
+++ src/pages/Income/components/accountantDeclarations/DeclarationPage.jsx
@@ -97,7 +97,7 @@ const DeclarationPage = () => {
     enabled: id !== undefined,
   });
 
-  const handleDownload = (pdf) => {};
+  const handleDownload = (pdf) => { };
 
   return (
     <div>
@@ -154,21 +154,20 @@ const DeclarationPage = () => {
               ?.filter((item) => {
                 return searchEmp
                   ? item.empId?.first_name
-                      .toLowerCase()
-                      .includes(searchEmp.toLowerCase()) ||
-                      item.empId?.last_name
-                        .toLowerCase()
-                        .includes(searchEmp.toLowerCase())
+                    .toLowerCase()
+                    .includes(searchEmp.toLowerCase()) ||
+                  item.empId?.last_name
+                    .toLowerCase()
+                    .includes(searchEmp.toLowerCase())
                   : item.empId;
               })
               .map((ele) => (
                 <Link
                   to={`/notification/income-tax/${ele.empId._id}`}
                   className={` px-6 my-1 mx-3 py-2 flex gap-2 rounded-md items-center hover:bg-gray-50
-                ${
-                  ele.empId._id === id &&
-                  "bg-blue-500 text-white hover:!bg-blue-300 "
-                }
+                ${ele.empId._id === id &&
+                    "bg-blue-500 text-white hover:!bg-blue-300 "
+                    }
                 `}
                 >
                   <Avatar />
@@ -177,9 +176,8 @@ const DeclarationPage = () => {
                       {ele.empId?.first_name} {ele?.empId.last_name}
                     </h1>
                     <h1
-                      className={`text-sm text-gray-500  ${
-                        ele.empId._id === id && "text-white"
-                      }`}
+                      className={`text-sm text-gray-500  ${ele.empId._id === id && "text-white"
+                        }`}
                     >
                       {ele.empId.email}
                     </h1>
@@ -190,9 +188,8 @@ const DeclarationPage = () => {
         </article>
 
         <article
-          className={`md:w-[75%] w-full flex flex-col min-h-[90vh] border-l-[.5px]  bg-gray-50 ${
-            !id && "md:!flex !hidden"
-          }`}
+          className={`md:w-[75%] w-full flex flex-col min-h-[90vh] border-l-[.5px]  bg-gray-50 ${!id && "md:!flex !hidden"
+            }`}
         >
           {empDataLoading ? (
             <div className="flex items-center justify-center my-2">
